95008faa72b34cbb910bf463d2d373b0387220cf
mani1 <mani1@NANI143>
2022-08-04 13:53:21 +0530

    Added program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UC4_ReverseNumber.CheckUC4_ReverseNumber();
+        }
+    }
+}
