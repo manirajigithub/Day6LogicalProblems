b7c0dcc0d81b0740d9fed25abe36c225fb159e06
mani1 <mani1@NANI143>
2022-08-04 13:22:56 +0530

    Added program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC2_PerfectNumber.CheckUC2_PerfectNumber();
+        }
+    }
+}
+
