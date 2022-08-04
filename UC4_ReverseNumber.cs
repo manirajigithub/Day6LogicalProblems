6e7daa3f3be567e9eb2e264d12d9ac2ccfa2d9b2
mani1 <mani1@NANI143>
2022-08-04 14:08:41 +0530

    Added Reverse Number Program

---
+++ UC4_ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    public static  class UC4_ReverseNumber
+    {
+        public static void CheckUC4_ReverseNumber()
+        {
+            Console.WriteLine("Reversing a Number");
+            int n = 50, calrem, mulvar, addrem, divnum;
+            for (int i = 1; i < n; i++)
+            {
+                calrem = n % i;
+                mulvar = calrem * 10;
+                addrem = calrem + mulvar;
+                divnum = addrem / 10;
+                Console.WriteLine(divnum);
+            }
+           
+        }
+    }
+}
