99d2cd2c8ce1fb8a1b5a6c59e93c15cd3b9ea0b4
mani1 <mani1@NANI143>
2022-08-04 13:07:08 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_FibonacciSeries.CheckUC1_FiboNacciSeries();
+        }
+    }
+}
+
