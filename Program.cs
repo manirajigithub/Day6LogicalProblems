e14d6b9fee13c10931d5145d9c7df5f849c0f07f
mani1 <mani1@NANI143>
2022-08-04 14:11:51 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CouponNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UC5_CouponNumber.CheckUC5_CouponNumber();
+        }
+    }
+}
