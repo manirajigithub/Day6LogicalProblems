a42b7b5ac86477e49cc885e02dbfa5de21706221
mani1 <mani1@NANI143>
2022-08-04 14:31:20 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UC6_StopWatch.CheckUC6_StopWatch();
+        }
+    }
+}
