c282af78a00b3deee42ff283f697f09e6ad6a439
mani1 <mani1@NANI143>
2022-08-04 13:37:54 +0530

    Added program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PrimeNumberNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UC3_PrimeNumber.CheckUC3_PrimeNumber();
+        }
+    }
+}
